143f8ecf9cd15cce74e897b21d22ff89af85e7ad
xander9290 <53929034+xander9290@users.noreply.github.com>
2025-06-22 16:53:28 -0600

    se crear top nav preview

--- app/(auth)/app/layout.tsx
+++ app/(auth)/app/layout.tsx
@@ -1,9 +1,17 @@
+import TopNav from "@/components/top-nav/TopNav";
+import { SessionProvider } from "next-auth/react";
+
 function LayoutApp({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return (
+    <SessionProvider>
+      <TopNav />
+      <main className="container vh-100">{children}</main>
+    </SessionProvider>
+  );
 }
 
 export default LayoutApp;
---
+++ components/top-nav/TopNav.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+
+function TopNav() {
+  const { data: session } = useSession();
+  return (
+    <Navbar expand="lg" bg="dark" data-bs-theme="dark" className="sticky-top">
+      <Container>
+        <Navbar.Brand as={Link} href="/app">
+          ERP LEAD
+        </Navbar.Brand>
+        <Navbar.Collapse />
+        <Nav className="me-auto"></Nav>
+        <Nav>
+          <NavDropdown
+            title={
+              <>
+                <i className="bi bi-person-circle me-2"></i>
+                <span className="text-capitalize">{session?.user?.name}</span>
+              </>
+            }
+            className="text-capitalize"
+          >
+            <NavDropdown.Item
+              as={Link}
+              href={`/app/users/${session?.user?.name}`}
+            >
+              mi perfil
+            </NavDropdown.Item>
+            <NavDropdown.Item onClick={() => signOut()} href="#">
+              cerrar sesión
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default TopNav;
